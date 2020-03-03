e8d4edb646e7248306917829113913c94c886097
DongBlack <2191314147@qq.com>
2020-03-03 09:29:42 +0800

    配置urls

--- day1/booktest/admin.py
+++ day1/booktest/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models   import  BookInfo
 from .models   import  HeroInfo
 # # Register your models here.
-admin.site.register(BookInfo)
-admin.site.register(HeroInfo)
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id','title','pub_date']
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display =  ['id','name','content','gender','book']
+
+
+admin.site.register(BookInfo,BookInfoAdmin)
+admin.site.register(HeroInfo,HeroInfoAdmin)
 
 
